1166c5d5c1b6ab93b19a1df88c01505bbd7463cc
wwek <licoolgo@gmail.com>
2015-10-22 23:07:57 +0800

    修改todo

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # ncgo
-一个用Golang写的网络瑞士军刀工具，类似于netcat
+网络瑞士军刀，Golang打造，类似于netcat
 
 # 已实现功能
+* 暂无
 
 # ToDo
 * 检查网络TCP UDP端口是否打开
 * 扫描IP
+* SpeedTest Cli 命令行模式带宽测速
 * http 和 socks代理
 * 反向tcp隧道
 * Ping
 * Mtr
+
+# 项目依赖
+github.com/codegangsta/cli
--- ncgo.go
+++ ncgo.go
@@ -7,10 +7,24 @@ import (
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "greet"
-	app.Usage = "fight the loneliness!"
+	app.Name = "ncgo"
+	app.Usage = "网络瑞士军刀，Golang打造"
+	app.Version = "v1.0"
+	app.Copyright = `
+	项目源码： https://github.com/wwek/ncgo
+
+	wwek|流水理鱼
+	http://www.iamle.com`
 	app.Action = func(c *cli.Context) {
-		println("Hello friend!")
+		name := "someone"
+		if len(c.Args()) > 0 {
+			name = c.Args()[0]
+		}
+		if c.String("lang") == "spanish" {
+			println("Hola", name)
+		} else {
+			println("Hello", name)
+		}
 	}
 
 	app.Run(os.Args)
