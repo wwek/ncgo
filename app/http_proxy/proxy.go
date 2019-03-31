d2e97d854567d4fb4dbe180a70ee9a75a70fa61c
wwek <licoolgo@gmail.com>
2019-04-01 07:45:46 +0800

    up

--- app/http_proxy/proxy.go
+++ app/http_proxy/proxy.go
@@ -1,24 +1,27 @@
 package main
 
 type Pxy struct {
-	Cfg
+	Cfg Cfg
 }
 
 // 设置type
 type Cfg struct {
-	Addr        string // 监听地址
-	Port        string // 监听端口
-	IsAnonymous bool   // 高匿名模式
-	BaseAuth           // 基础认证
+	Addr        string   // 监听地址
+	Port        string   // 监听端口
+	IsAnonymous bool     // 高匿名模式
+	BaseAuth    BaseAuth // 基础认证
 }
 
 // 实例化
 func NewPxy() *Pxy {
 	return &Pxy{
-		Cfg{
-			Addr:        "0.0.0.0",
+		Cfg: Cfg{
+			Addr:        "",
 			Port:        "8081",
 			IsAnonymous: true,
+			BaseAuth: BaseAuth{
+				IsBaseAuth: false,
+			},
 		},
 	}
 }
@@ -34,12 +37,12 @@ func (p *Pxy) SetPxyCfg(cfg Cfg) {
 	if cfg.IsAnonymous != p.Cfg.IsAnonymous {
 		p.Cfg.IsAnonymous = cfg.IsAnonymous
 	}
-	if cfg.IsBaseAuth != p.Cfg.IsBaseAuth {
-		p.Cfg.IsBaseAuth = cfg.IsBaseAuth
+	if cfg.BaseAuth.IsBaseAuth != p.Cfg.BaseAuth.IsBaseAuth {
+		p.Cfg.BaseAuth.IsBaseAuth = cfg.BaseAuth.IsBaseAuth
 	}
-	if cfg.UserName != "" && cfg.PassWord != "" {
-		p.Cfg.UserName = cfg.UserName
-		p.Cfg.PassWord = cfg.PassWord
+	if cfg.BaseAuth.UserName != "" && cfg.BaseAuth.PassWord != "" {
+		p.Cfg.BaseAuth.UserName = cfg.BaseAuth.UserName
+		p.Cfg.BaseAuth.PassWord = cfg.BaseAuth.PassWord
 	}
 
 }
